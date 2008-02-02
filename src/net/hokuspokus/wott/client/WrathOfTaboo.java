42e48cb6ed2814dcd57a5209f2762fd8e2bbf08f
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-02 00:33:10 +0000

    Now with geometry

--- src/net/hokuspokus/wott/client/WrathOfTaboo.java
+++ src/net/hokuspokus/wott/client/WrathOfTaboo.java
@@ -1,13 +1,18 @@
 package net.hokuspokus.wott.client;
 
 import net.hokuspokus.wott.common.Board;
+import net.hokuspokus.wott.common.Person;
+import net.hokuspokus.wott.common.Player;
 
 import com.jme.app.SimpleGame;
+import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Node;
 
 public class WrathOfTaboo extends SimpleGame
 {
 	Board board;
+	Player p1;
+	Player p2;
 	
 	public static void main(String[] args)
 	{
@@ -18,7 +23,25 @@ public class WrathOfTaboo extends SimpleGame
 	protected void simpleInitGame()
 	{
 		rootNode = new Node();
+		createNewBoard();
+	}
+	
+	
+	private void createNewBoard()
+	{
+		board = new Board();
+		p1 = new Player(new ColorRGBA(1,0,0,1));
+		p2 = new Player(new ColorRGBA(0,0,1,1));
 		
+		for(Person person : p1.getPopulation())
+		{
+			rootNode.attachChild(person.getGeomtry());
+		}
+		
+		for(Person person : p2.getPopulation())
+		{
+			rootNode.attachChild(person.getGeomtry());
+		}
 	}
 	
 	@Override
