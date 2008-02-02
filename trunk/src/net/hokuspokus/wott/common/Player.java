ba07f11989e1406df49bdf3d85e53a86b892c821
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-02 16:45:32 +0000

    Nu med bobbing

--- trunk/src/net/hokuspokus/wott/common/Player.java
+++ trunk/src/net/hokuspokus/wott/common/Player.java
@@ -64,7 +64,12 @@ public class Player
 					else
 						r1.getLocalScale().set(Vector3f.UNIT_XYZ);
 					*/
-					r1 = new Node();
+					//r1 = new Node();
+	            	File file = new File("ressources/3d gfx/Bobbing.jme");
+	            	SimpleResourceLocator locator = new SimpleResourceLocator(file.getParentFile().toURI());
+	                ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, locator);
+					r1 = (Node) BinaryImporter.getInstance().load(file);
+					//r1.getLocalScale().set(0.02f, 0.02f, 0.02f);
 				}
 				else
 				{
