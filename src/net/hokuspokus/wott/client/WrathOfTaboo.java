65f6fbacaceea2d88a8c9f940e229d8db4eeed87
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-02 00:18:17 +0000

    sljdflksjf

---
+++ src/net/hokuspokus/wott/client/WrathOfTaboo.java
@@ -0,0 +1,35 @@
+package net.hokuspokus.wott.client;
+
+import net.hokuspokus.wott.common.Board;
+
+import com.jme.app.SimpleGame;
+import com.jme.scene.Node;
+
+public class WrathOfTaboo extends SimpleGame
+{
+	Board board;
+	
+	public static void main(String[] args)
+	{
+		new WrathOfTaboo().start();
+	}
+	
+	@Override
+	protected void simpleInitGame()
+	{
+		rootNode = new Node();
+		
+	}
+	
+	@Override
+	protected void simpleRender()
+	{
+		super.simpleRender();
+	}
+	
+	@Override
+	protected void simpleUpdate()
+	{
+		super.simpleUpdate();
+	}
+}
