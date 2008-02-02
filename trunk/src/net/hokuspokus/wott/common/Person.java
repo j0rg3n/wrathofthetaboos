766a0a6c83053027549b4ca2a75a71bc72561805
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-02 00:33:10 +0000

    Now with geometry

--- trunk/src/net/hokuspokus/wott/common/Person.java
+++ trunk/src/net/hokuspokus/wott/common/Person.java
@@ -1,6 +1,8 @@
 package net.hokuspokus.wott.common;
 
 import com.jme.math.Vector2f;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
 
 public class Person
 {
@@ -14,11 +16,18 @@ public class Person
 	PersonType type;
 	Vector2f pos;
 	final int id;
+	Spatial geometry;
 	
 	public Person(Player owner, PersonType type)
 	{
 		this.owner = owner;
 		this.type = type;
 		this.id = nextId++;
+		this.geometry = owner.createNode(type);
+	}
+
+	public Spatial getGeomtry()
+	{
+		return geometry;
 	}
 }
--- trunk/src/net/hokuspokus/wott/common/Player.java
+++ trunk/src/net/hokuspokus/wott/common/Player.java
@@ -3,6 +3,11 @@ package net.hokuspokus.wott.common;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+import com.jme.scene.shape.Box;
+
 import net.hokuspokus.wott.common.Person.PersonType;
 
 public class Player
@@ -10,10 +15,11 @@ public class Player
 	private static final int MANCOUNT = 15;
 	private static final int WOMANCOUNT = 15;
 	
+	private ColorRGBA color;
 	List<Person> people = new ArrayList<Person>();
 
-	public Player() {
-		
+	public Player(ColorRGBA color) {
+		this.color = color;
 		for (int i = 0; i < MANCOUNT; ++i) {
 			people.add(new Person(this, PersonType.MAN));
 		}
@@ -21,5 +27,15 @@ public class Player
 		for (int i = 0; i < WOMANCOUNT; ++i) {
 			people.add(new Person(this, PersonType.WOMAN));
 		}
+	}
+
+	public Spatial createNode(PersonType type)
+	{
+		return new Box();
+	}
+
+	public Iterable<Person> getPopulation()
+	{
+		return people;
 	}
 }
