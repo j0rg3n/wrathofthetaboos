4ee055444a9cf63069a4a3d5c9cea19d152f14d6
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-03 15:13:24 +0000

    - Now with music converted and a working deploy

--- trunk/compile_zip.sh
+++ trunk/compile_zip.sh
@@ -1,1 +1,10 @@
-zip JealousGods.zip -r start.sh start.bat release/JealousGods.jar jme/lib/* ressources/2d\ gfx/*  ressources/3d\ gfx/*.jme
+zip JealousGods.zip -r \
+	start.sh start.bat \
+	release/JealousGods.jar \
+	jme/lib/* \
+	ressources/2d\ gfx/*  \
+	ressources/3d\ gfx/*.jme \
+	ressources/3d\ gfx/*.JPG \
+	ressources/sound/*.wav \
+	ressources/sound/*.ogg \
+	press/press.jpg
