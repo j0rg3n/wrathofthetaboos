680e5a33737df324f72e82459e82715611cb20a6
greisen <greisen@b760afd0-1f45-0410-b975-7103d5bbc06f>
2008-02-03 00:12:45 +0000

    Music and going the right way

--- trunk/src/net/hokuspokus/wott/common/Player.java
+++ trunk/src/net/hokuspokus/wott/common/Player.java
@@ -45,69 +45,18 @@ public class Player
 			return r1;
 		} else {
 			Node r1 = null;
-				if(type == PersonType.MAN)
-				{
-					/*
-					URL model = Player.class.getClassLoader().getResource("ressources/3d gfx/" + (type == PersonType.WOMAN ? "mini_negerkvinde.3DS" : "Mand2.3DS"));
-	
-					MaxToJme C1 = new MaxToJme();
-					ByteArrayOutputStream BO = new ByteArrayOutputStream();
-					C1.convert(new BufferedInputStream(model.openStream()), BO);
-		            
-					r1 = (Node) BinaryImporter.getInstance().load(new ByteArrayInputStream(BO.toByteArray()));
-					if(type != PersonType.WOMAN)
-						r1.setLocalScale(.010f);
-					else
-						r1.getLocalScale().set(Vector3f.UNIT_XYZ);
-					*/
-					//r1 = new Node();
-	            	//File file = new File("ressources/3d gfx/Bobbing2.jme");
-	            	//SimpleResourceLocator locator = new SimpleResourceLocator(file.getParentFile().toURI());
-	                //ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, locator);
-					//r1 = (Node) BinaryImporter.getInstance().load(file);
-					r1 = (Node) NodeUtils.loadNode("ressources/3d gfx/negermand.jme");
-					r1.getLocalScale().set(0.22f, 0.22f, 0.22f);
-				}
-				else
-				{
-	            	//File file = new File("ressources/3d gfx/mini_negerkvinde03.jme");
-	            	//SimpleResourceLocator locator = new SimpleResourceLocator(file.getParentFile().toURI());
-	                //ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, locator);
-					//r1 = (Node) BinaryImporter.getInstance().load(file);
-					
-					r1 = (Node) NodeUtils.loadNode("ressources/3d gfx/negerkvinde.jme");
-					r1.getLocalScale().set(0.22f, 0.22f, 0.22f);
-					//r1 = new Node();
-				}
+			
+			if(type == PersonType.MAN)
+			{
+				r1 = (Node) NodeUtils.loadNode("ressources/3d gfx/negermand2.jme");
+				r1.getLocalScale().set(0.22f, 0.22f, 0.22f);
+			}
+			else
+			{
+				r1 = (Node) NodeUtils.loadNode("ressources/3d gfx/negerkvinde.jme");
+				r1.getLocalScale().set(0.22f, 0.22f, 0.22f);
+			}
 				
-				//TextureUtil.getInstance().setTexture(r1, "/ressources/2d gfx/player_"+color+".jpg");
-				
-				/*
-				MaterialState ms = DisplaySystem.getDisplaySystem().getRenderer().createMaterialState();
-				ms.setColorMaterial(MaterialState.CM_NONE);
-				r1.setRenderState(ms);
-				r1.updateRenderState();
-				
-*/				
-/*
-				for (Controller c : r1.getControllers())
-				{
-					System.out.println(" ");
-					System.out.println(c);
-					System.out.println(" ");
-				}
-				System.out.println();
-
-				for (Spatial c : r1.getChildren())
-				{
-					for (Controller con : c.getControllers())
-					{
-						System.out.println(" ");
-						System.out.println(con);
-						System.out.println(" ");
-					}
-				}
-*/
 			SpatialTransformer kc;
 			if (r1 != null && r1.getControllerCount() >= 1 && r1.getController(0) != null)
 			{
